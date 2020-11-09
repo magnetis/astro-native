0cf3b56f5985caac1ce3a1d16bd911e94140f084
Alysson Lopes <alyssondlopes@gmail.com>
2020-11-09 18:52:06 -0300

    Transform project in RN app

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+/**
+ * @format
+ */
+
 import { AppRegistry } from 'react-native';
-import Storybook from './storybook';
+import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Storybook);
+AppRegistry.registerComponent(appName, () => App);
