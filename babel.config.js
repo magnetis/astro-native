37152488de4ccfa663717e0fb6868254d43fea1d
Alysson Lopes <alyssondlopes@gmail.com>
2020-11-09 18:52:06 -0300

    Transform project in RN app

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        cwd: 'babelrc',
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.ios.js', '.android.js'],
-        alias: {
-          '@tokens': './src/tokens',
-          '@components': './src/components',
-          '@typings': './typings',
-          '@hooks': './src/hooks',
-          '@root': './',
-        },
-      },
-    ],
-  ],
 };
