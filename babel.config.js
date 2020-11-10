136d555ed83dc5b9becb8cef994daee2237321a3
Alysson Lopes <alyssondlopes@gmail.com>
2020-11-10 17:38:28 -0300

    Add aliases to project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.ios.js', '.android.js'],
+        alias: {
+          '@tokens': './src/tokens',
+          '@components': './src/components',
+          '@typings': './typings',
+        },
+      },
+    ],
+  ],
 };
