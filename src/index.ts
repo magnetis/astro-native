0f76a51059adbe7c1bd39252feca3d0b68311f44
Alysson Lopes <alyssondlopes@gmail.com>
2020-11-10 17:48:18 -0300

    Add tokens

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('olá mundo');
+import gradients from '@tokens/gradients';
+import zIndex from '@tokens/z-index';
+
+/**
+ * Tokens
+ */
+export const tokens = {
+  gradients,
+  ...zIndex,
+};
+export type { Gradient, GradientMap, GradientID } from '@tokens/gradients';
+
