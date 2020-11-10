09870409f945d9bc2b58be0bcf322d9f17f4c941
Alysson Lopes <alyssondlopes@gmail.com>
2020-11-10 17:48:18 -0300

    Add tokens

---
+++ src/tokens/fonts.ts
@@ -0,0 +1,8 @@
+export const poppins = 'Poppins-Regular';
+export const poppinsMedium = 'Poppins-Medium';
+export const poppinsSemiBold = 'Poppins-SemiBold';
+export const poppinsBold = 'Poppins-Bold';
+export const poppinsBlack = 'Poppins-Black';
+
+export const lato = 'Lato-Regular';
+export const latoBold = 'Lato-Bold';
