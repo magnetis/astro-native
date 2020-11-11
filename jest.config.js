ab4b1de31560a4111f83fc4b968d1103efda69ec
Alysson Lopes <alyssondlopes@gmail.com>
2020-11-11 11:22:20 -0300

    chore: configure tests, add styled-components

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,11 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>setupJest.ts'],
+  setupFilesAfterEnv: ['<rootDir>setupJest.ts', '@testing-library/jest-native/extend-expect'],
   preset: 'react-native',
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).ts?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
   transformIgnorePatterns: ['node_modules/(?!react-native-svg|react-native)/'],
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
-  },
   coverageThreshold: {
     branches: 100,
     functions: 100,
--- setupJest.ts
+++ setupJest.ts
@@ -18,14 +18,3 @@ jest.mock('react-native/Libraries/LogBox/Data/LogBoxData', () => {
     },
   };
 });
-
-// Mocks for react navigation
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
-
-  // The mock for `call` immediately calls the callback which is incorrect
-  // So we override it with a no-op
-  Reanimated.default.call = () => {};
-
-  return Reanimated;
-});
