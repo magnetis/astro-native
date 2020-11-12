da7ede8aa93d32bf6e21bf0de504c5efdf934fa9
Alysson Lopes <LopesAlysson@users.noreply.github.com>
2020-11-12 18:59:57 -0300

    Implement Text component (#2)
    * Implement text components
    * Add conventional-changelog
    * Fix eslint config to work in Travis properly
    
    Co-authored-by: Tobias Ulrich <tobiasbulrich@gmail.com>

---
+++ src/components/Text/index.ts
@@ -0,0 +1,17 @@
+import { PrimaryTextVerySmall, PrimaryTextSmall, PrimaryTextMedium, PrimaryTextLarge } from './PrimaryText';
+import type { PrimaryTextProps } from './PrimaryText';
+
+import { SecondaryTextVerySmall, SecondaryTextSmall, SecondaryTextMedium, SecondaryTextLarge } from './SecondaryText';
+import type { SecondaryTextProps } from './SecondaryText';
+
+export type { PrimaryTextProps, SecondaryTextProps };
+export {
+  PrimaryTextVerySmall,
+  PrimaryTextSmall,
+  PrimaryTextMedium,
+  PrimaryTextLarge,
+  SecondaryTextVerySmall,
+  SecondaryTextSmall,
+  SecondaryTextMedium,
+  SecondaryTextLarge,
+};
--- src/stories/Text.stories.tsx
+++ src/stories/Text.stories.tsx
@@ -1,6 +1,57 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 
-import Text from '../components/Text';
+import {
+  PrimaryTextVerySmall,
+  PrimaryTextSmall,
+  PrimaryTextMedium,
+  PrimaryTextLarge,
+  SecondaryTextVerySmall,
+  SecondaryTextSmall,
+  SecondaryTextMedium,
+  SecondaryTextLarge,
+} from '../components/Text';
+import StoryDecorator from './StoryDecorator';
 
-storiesOf('Text', module).add('default', () => <Text text="Typescript works!" />);
+storiesOf('Primary Text', module)
+  .addDecorator(StoryDecorator)
+  .addDecorator(withKnobs)
+  .add('Large', () => (
+    <PrimaryTextLarge bold={boolean('bold', false)} semiBold={boolean('semiBold', false)}>
+      The quick brown fox jumps over the lazy dog
+    </PrimaryTextLarge>
+  ))
+  .add('Medium', () => (
+    <PrimaryTextMedium bold={boolean('bold', false)} semiBold={boolean('semiBold', false)}>
+      The quick brown fox jumps over the lazy dog
+    </PrimaryTextMedium>
+  ))
+  .add('Small', () => (
+    <PrimaryTextSmall bold={boolean('bold', false)} semiBold={boolean('semiBold', false)}>
+      The quick brown fox jumps over the lazy dog
+    </PrimaryTextSmall>
+  ))
+  .add('Very Small', () => (
+    <PrimaryTextVerySmall bold={boolean('bold', false)} semiBold={boolean('semiBold', false)}>
+      The quick brown fox jumps over the lazy dog
+    </PrimaryTextVerySmall>
+  ));
+
+storiesOf('Secondary Text', module)
+  .addDecorator(StoryDecorator)
+  .addDecorator(withKnobs)
+  .add('Large', () => (
+    <SecondaryTextLarge bold={boolean('bold', false)}>The quick brown fox jumps over the lazy dog</SecondaryTextLarge>
+  ))
+  .add('Medium', () => (
+    <SecondaryTextMedium bold={boolean('bold', false)}>The quick brown fox jumps over the lazy dog</SecondaryTextMedium>
+  ))
+  .add('Small', () => (
+    <SecondaryTextSmall bold={boolean('bold', false)}>The quick brown fox jumps over the lazy dog</SecondaryTextSmall>
+  ))
+  .add('Very Small', () => (
+    <SecondaryTextVerySmall bold={boolean('bold', false)}>
+      The quick brown fox jumps over the lazy dog
+    </SecondaryTextVerySmall>
+  ));
