29148d5bb71e830bff249e4695afdde5c2cc0c4f
Alysson Lopes <LopesAlysson@users.noreply.github.com>
2020-11-12 18:59:57 -0300

    Implement Text component (#2)
    * Implement text components
    * Add conventional-changelog
    * Fix eslint config to work in Travis properly
    
    Co-authored-by: Tobias Ulrich <tobiasbulrich@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ import zIndex from '@tokens/z-index';
 /**
  * Tokens
  */
+
 export const tokens = {
   gradients,
-  ...zIndex,
+  zIndex,
 };
 export type { Gradient, GradientMap, GradientID } from '@tokens/gradients';
 
+/**
+ * Components
+ */
+
+export * from '@components/Text';
+export type { PrimaryTextProps, SecondaryTextProps } from '@components/Text';
