e2a601e929fce9e0aca604ff8717478914d54bee
Alysson Lopes <alyssondlopes@gmail.com>
2020-11-27 15:53:40 -0300

    feat(Inputs): Implement InputEyeToggle

---
+++ src/components/Inputs/InputEyeToggle.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Pressable, StyleSheet } from 'react-native';
+import { colors } from '@magnetis/astro-galaxy-tokens';
+
+import { EyeClosedIcon, EyeOpenIcon } from '@components/Icons';
+
+interface InputEyeToggleProps {
+  visible: boolean;
+  onPress: () => void;
+  large: boolean;
+}
+
+function InputEyeToggle({ visible, onPress, large }: InputEyeToggleProps) {
+  const iconProps = { color: colors.moon900, width: 32, height: 32 };
+
+  if (visible) {
+    return (
+      <Pressable testID="InputEyeToggle.EyeOpen" onPress={onPress} style={[styles.iconView, { top: large ? 16 : 12 }]}>
+        <EyeOpenIcon {...iconProps} />
+      </Pressable>
+    );
+  }
+
+  return (
+    <Pressable
+      testID="InputEyeToggle.EyeClosed"
+      onPress={onPress}
+      hitSlop={10}
+      style={[styles.iconView, { top: large ? 16 : 12 }]}
+    >
+      <EyeClosedIcon {...iconProps} />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  iconView: {
+    position: 'absolute',
+    right: 16,
+    backgroundColor: 'red',
+  },
+});
+
+export default InputEyeToggle;
