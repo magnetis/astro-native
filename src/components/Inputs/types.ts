e634fb96fc32c60fd0e9b427ff1c62c62bb406b8
Alysson Lopes <LopesAlysson@users.noreply.github.com>
2020-11-27 17:35:42 -0300

    Implement TextInput (#9)
    * refactor: move getFontSize utility function to tokens directory, in order to be more accessible in every part of the project
    
    * feat(hooks): Implement useDidMount hook
    
    * fix(RadioGroup): remove usage of useMemo
    
    * chore(tests): Change coverage threshold to 95%
    
    * feat(Inputs): Implement InputErrorMessage
    
    * feat(Inputs): Implement InputEyeToggle
    
    * feat(Inputs): Implement InputStatusIcon
    
    * feat(Inputs): Implement InputLabel
    
    * feat(Inputs): Implement TextInput

--- src/components/Inputs/types.ts
+++ src/components/Inputs/types.ts
@@ -1,24 +1,14 @@
 import type { TextInputProps } from 'react-native';
 
 export interface BaseInputProps extends TextInputProps {
-  /** Input change callback, returns value as a parameter */
   onChangeText: (value: string) => void;
-  /** Label text to be displayed on input */
   label: string;
-  /** Error message or empty string when no error */
   error?: string;
-  /** Show validated visual feedback when true */
   validated?: boolean;
-  /** Disable any user interaction with input */
   disabled?: boolean;
-  /** Does not show error message when false */
   touched?: boolean;
-  /** Input blur callback */
   onBlur?: () => void;
-  /** Input focus callback */
   onFocus?: () => void;
-  /** Render input's large size when true */
   large?: boolean;
-  /** Used to locate this component in end-to-end tests */
   testID?: string;
 }
