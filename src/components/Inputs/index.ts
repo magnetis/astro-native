dbe27aa4fb43c3bac57d73f3e0596bc586d5e9f3
Alysson Lopes <LopesAlysson@users.noreply.github.com>
2020-12-02 16:11:22 -0300

    Implement ControlInput (#11)
    * fix(TextInput): change paddingRight condition from validated to isValidated
    
    * feat(Inputs): Implement ControlInput

--- src/components/Inputs/index.ts
+++ src/components/Inputs/index.ts
@@ -1,5 +1,7 @@
 export { default as TextInput } from './TextInput';
 export { default as MaskedInput } from './MaskedInput';
+export { default as ControlInput } from './ControlInput';
 
 export type { TextInputProps } from './TextInput';
 export type { MaskedInputProps } from './MaskedInput';
+export type { ControlInputProps } from './ControlInput';
