d0446fc2329dc73f59897d7a735166d7180ed92e
Alysson Lopes <LopesAlysson@users.noreply.github.com>
2020-12-03 09:10:47 -0300

    Write Input docs (#12)
    * fix(travis): Add yarn to run tsc on travisci
    
    * docs(Input): Write JSDOC to input components
    
    * Update src/components/Inputs/ControlInput.tsx

--- src/components/Inputs/types.ts
+++ src/components/Inputs/types.ts
@@ -1,14 +1,24 @@
 import type { TextInputProps } from 'react-native';
 
 export interface BaseInputProps extends TextInputProps {
+  /** Input change callback, returns value as a parameter */
   onChangeText: (value: string) => void;
+  /** Label text to be displayed on input */
   label: string;
+  /** Error message or empty string when no error */
   error?: string;
+  /** Show validated visual feedback when true */
   validated?: boolean;
+  /** Disable any user interaction with input */
   disabled?: boolean;
+  /** Does not show error message when false */
   touched?: boolean;
+  /** Input blur callback */
   onBlur?: () => void;
+  /** Input focus callback */
   onFocus?: () => void;
+  /** Render input's large size when true */
   large?: boolean;
+  /** TestID string for testing */
   testID?: string;
 }
