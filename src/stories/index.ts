66d92e11cb60112dc6f0cc39a3ee38bbfd9a96cc
Alysson Lopes <alyssondlopes@gmail.com>
2020-12-16 19:35:43 -0300

    refactor(Inputs): move stories inside component folder

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -2,4 +2,4 @@ import './Buttons.stories';
 import './Text.stories';
 import './ControlsToggles.stories';
 import './Icons.stories';
-import './Inputs.stories';
+import '@components/Inputs/stories';
