4f0cfa2c44760b402e48b17bb4e1400ac1df807f
Renato Freire <contato.renatofreire@gmail.com>
2020-12-23 17:05:25 -0300

    feat(Title): Implement Title component

---
+++ src/components/Text/stories/Title.story.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { select } from '@storybook/addon-knobs';
+import { colors } from '@magnetis/astro-galaxy-tokens';
+
+import { TitleSmall, TitleMedium, TitleLarge } from '..';
+import { colorOptions } from '@root/storybook/options';
+
+const titleStories = storiesOf('Title', module);
+
+titleStories.add('Large', () => (
+  <TitleLarge color={select('color', colorOptions, colors.moon900)}>
+    The quick brown fox jumps over the lazy dog
+  </TitleLarge>
+));
+
+titleStories.add('Medium', () => (
+  <TitleMedium color={select('color', colorOptions, colors.moon900)}>
+    The quick brown fox jumps over the lazy dog
+  </TitleMedium>
+));
+
+titleStories.add('Small', () => (
+  <TitleSmall color={select('color', colorOptions, colors.moon900)}>
+    The quick brown fox jumps over the lazy dog
+  </TitleSmall>
+));
