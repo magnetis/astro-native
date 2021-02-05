f477af2622f6b3d060053d7302d2f0f340e6798e
Pedro Antonio Cerdeirinha <pedro.cerdera@hotmail.com>
2021-02-05 12:08:53 -0300

    fix(basebutton): min hitslop seted when button is smaller then 48
    small buttons has width and heith smaller than 48X48 and sometimes it is hard to press

--- src/components/Buttons/BaseButton.tsx
+++ src/components/Buttons/BaseButton.tsx
@@ -1,5 +1,13 @@
-import React, { ReactElement } from 'react';
-import { View, StyleSheet, ActivityIndicator, Pressable, PressableProps } from 'react-native';
+import React, { ReactElement, useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Pressable,
+  PressableProps,
+  LayoutChangeEvent,
+  Insets,
+} from 'react-native';
 
 import { getBorderRadius, getButtonPadding } from './utils';
 import { ButtonSize } from './types';
@@ -36,6 +44,10 @@ const styles = StyleSheet.create({
   },
 });
 
+type HitSlop = null | Insets | number;
+
+const MIN_HIT_SLOP = 48;
+
 function BaseButton({
   loading = false,
   disabled = false,
@@ -47,6 +59,8 @@ function BaseButton({
   hasIcon = false,
   ...props
 }: BaseButtonProps) {
+  const [hitSlop, setHitSlop] = useState<HitSlop>({ top: 0, bottom: 0, left: 0, right: 0 });
+
   const computedStyles: any = {
     ...getButtonPadding(size, { noHorizontalPadding, hasIcon }),
     backgroundColor: props.backgroundColor,
@@ -66,10 +80,35 @@ function BaseButton({
     disabled: disabled,
     style: [styles.button, computedStyles],
     testID: props.testID,
+    hitSlop: typeof props.hitSlop !== 'undefined' ? props.hitSlop : hitSlop,
   };
 
+  function adjustHitSlop(event: LayoutChangeEvent) {
+    const { width, height } = event.nativeEvent.layout;
+    if (width < MIN_HIT_SLOP || height < MIN_HIT_SLOP) {
+      const horizontalOffset = width < MIN_HIT_SLOP ? (MIN_HIT_SLOP - width) / 2 : 0;
+      const verticalOffset = height < MIN_HIT_SLOP ? (MIN_HIT_SLOP - height) / 2 : 0;
+      const newHitSlop = {
+        top: verticalOffset,
+        bottom: verticalOffset,
+        left: horizontalOffset,
+        right: horizontalOffset,
+      };
+      setHitSlop(newHitSlop);
+    }
+  }
+
+  function onLayoutButton(event: LayoutChangeEvent) {
+    if (props.onLayout) {
+      props.onLayout(event);
+    }
+    if (typeof props.hitSlop === 'undefined') {
+      adjustHitSlop(event);
+    }
+  }
+
   return (
-    <Pressable accessibilityRole="button" {...props} {...pressableProps}>
+    <Pressable accessibilityRole="button" {...props} {...pressableProps} onLayout={onLayoutButton}>
       <View style={{ alignItems: 'center', justifyContent: 'center', flex: fill ? 1 : 0 }}>
         <View style={{ opacity: loading ? 0 : 1 }}>{children}</View>
         {loading && (
