e4a8e3708516242ccaad23f4408ea871ff434e69
Alysson Lopes <alyssondlopes@gmail.com>
2021-02-09 15:40:29 -0300

    fix(tabs): fix UI when has single item
    Before this fix, tab item was stretching to fill tab container.

--- src/components/ControlsToggles/Tabs.tsx
+++ src/components/ControlsToggles/Tabs.tsx
@@ -162,7 +162,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
+    flex: 0,
   },
   itemWithBorderBottom: {
     borderRadius: 0,
