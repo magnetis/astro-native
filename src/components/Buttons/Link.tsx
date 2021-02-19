a0fb23e71b4543f7325da9927ee83126585a3727
julianosbento <juliano.bento@magnetis.com.br>
2021-02-19 16:41:26 -0300

    feat(link): add color prop

--- src/components/Buttons/Link.tsx
+++ src/components/Buttons/Link.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement } from 'react';
 import { TextProps } from 'react-native';
 
-import { isValidTextComponent } from './utils';
+import type { ButtonColor } from './types';
+import { getButtonMainColor, isValidTextComponent } from './utils';
 
 interface LinkProps extends TextProps {
   children: ReactElement;
   /** Color to be used as background color of button. Defaults to `"uranus"`. */
+  color?: ButtonColor;
   /** Link press callback */
   onPress: () => void;
 }
@@ -13,10 +15,11 @@ interface LinkProps extends TextProps {
 /**
  * For inline links in paragraphs and informative texts. Regular links navigate to other pages or external content.
  */
-function Link({ children, onPress, ...props }: LinkProps) {
+function Link({ children, onPress, color = 'uranus', ...props }: LinkProps) {
   return isValidTextComponent(children)
     ? React.cloneElement(children, {
         ...props,
+        color: getButtonMainColor(color),
         onPress,
         style: { textDecorationLine: 'underline' },
       })
