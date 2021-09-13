2dd5b03b6277f842114d9ca778cdec8ad3e32188
julianosbento <juliano.bento@magnetis.com.br>
2021-09-13 15:58:52 -0300

    refactor(storybook): change default story background color

--- storybook/StoryDecorator.tsx
+++ storybook/StoryDecorator.tsx
@@ -1,9 +1,10 @@
+import { colors } from '@magnetis/astro-tokens';
 import React from 'react';
 import { View } from 'react-native';
 
 export default function StoryDecorator(Story: any) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 8 }}>
+    <View style={{ flex: 1, justifyContent: 'center', padding: 8, backgroundColor: colors.solidBrightLight }}>
       <Story />
     </View>
   );
