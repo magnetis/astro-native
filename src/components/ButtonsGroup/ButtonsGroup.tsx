15e1bb5ed10b31ccf1221d052e246c9d4a34b363
Alberto Zaranza <albertozaranza@outlook.com>
2021-09-28 17:38:24 -0300

    feat(buttons-group): create buttons group

---
+++ src/components/ButtonsGroup/ButtonsGroup.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { FlatList, PressableProps, StyleSheet, View } from 'react-native';
+import { sizes } from '@magnetis/astro-tokens';
+
+import Button from '@components/Buttons/Button';
+
+type Item = {
+  value: string;
+  onPressItem: () => void;
+};
+
+export interface ButtonsGroupProps extends PressableProps {
+  items: Item[];
+  inversed?: boolean;
+}
+
+function ButtonsGroup({ testID, items, inversed, ...props }: ButtonsGroupProps) {
+  const [activeItem, setActiveItem] = useState(0);
+
+  return (
+    <FlatList
+      testID={testID}
+      data={items}
+      style={styles.container}
+      contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+      horizontal
+      bounces={false}
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(item) => item.value}
+      ItemSeparatorComponent={() => <View style={styles.itemContainer} />}
+      renderItem={({ item, index }) => (
+        <Button
+          {...props}
+          accessibilityLabel={item.value}
+          size="small"
+          type={activeItem === index ? 'solid' : 'subtle'}
+          variant={activeItem === index ? 'primary' : inversed ? 'inversed' : 'secondary'}
+          text={item.value}
+          onPress={() => {
+            setActiveItem(index);
+            item.onPressItem();
+          }}
+        />
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 0,
+  },
+  itemContainer: {
+    marginLeft: sizes.nano,
+  },
+});
+
+export default ButtonsGroup;
--- storybook/stories.ts
+++ storybook/stories.ts
@@ -1,2 +1,3 @@
 import '@components/Buttons/stories';
+import '@components/ButtonsGroup/stories';
 import '@components/Icons/stories';
