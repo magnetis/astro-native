0c5ddf048755cf4388c8bfaf10471ff709eeb4da
Michele-Camargo <mi.farias98@gmail.com>
2022-05-26 16:37:56 -0300

    refactor(icons): remove icons of dashboard and support, remove duplicate icon "search"

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,14 +1,11 @@
-export * from './Dashboard';
-export * from './Support';
-// Temporarily commented out to avoid conflict with the existing icon with the same name. Already have a prioritized task to delete legacy icons
-// export * from './Arrows';
-// export * from './Informations';
-// export * from './Essentials';
-// export * from './Documents';
-// export * from './Communications';
-// export * from './Finances';
-// export * from './Security';
-// export * from './Social';
-// export * from './Medias';
+export * from './Arrows';
+export * from './Informations';
+export * from './Essentials';
+export * from './Documents';
+export * from './Communications';
+export * from './Finances';
+export * from './Security';
+export * from './Social';
+export * from './Medias';
 
 export type { IconProps, IconID } from './types';
