152e4c6210f30db4c724ff266fdbc477973c5d21
Pedro <henrique.pedro.1992@gmail.com>
2023-04-10 15:28:47 -0300

    feat(JournalIcon): add journal icon

--- src/components/Icons/Communications/index.ts
+++ src/components/Icons/Communications/index.ts
@@ -1,9 +1,10 @@
 export { default as CallIcon } from './Call';
 export { default as EmailIcon } from './Email';
-export { default as EmailAdressIcon } from './EmailAdress';
+export { default as EmailAddressIcon } from './EmailAddress';
 export { default as EmailOpenIcon } from './EmailOpen';
 export { default as EmailSendIcon } from './EmailSend';
 export { default as MessageIcon } from './Message';
 export { default as MessageAltIcon } from './MessageAlt';
 export { default as MessageQuestionIcon } from './MessageQuestion';
 export { default as SupportIcon } from './Lifebuoy';
+export { default as JournalIcon } from './Journal';
