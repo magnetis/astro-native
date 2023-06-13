581f901e3ad6b935f88e8c4d6b8e17a643f7cb5a
Luciano Lima <luciano.lima@magnetis.com.br>
2023-06-13 14:16:15 -0300

    feat(link): allow use any color (#719)
    * fix(story): link button story does not having color select
    
    * feat(link): allow use any color

--- src/components/Buttons/stories/Link.story.tsx
+++ src/components/Buttons/stories/Link.story.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
+import { text } from '@storybook/addon-knobs';
 
-import { Link } from '..';
 import { PrimaryTextMedium } from '@components/Text';
 
+import { Link } from '..';
+
 storiesOf('Buttons', module).add('Link', () => (
-  <Link onPress={() => console.log('Pressed')}>
-    <PrimaryTextMedium>Button</PrimaryTextMedium>
+  <Link color={text('color', 'uranus')} onPress={() => console.log('Pressed')}>
+    <PrimaryTextMedium>{text('text', 'Button')}</PrimaryTextMedium>
   </Link>
 ));
 
